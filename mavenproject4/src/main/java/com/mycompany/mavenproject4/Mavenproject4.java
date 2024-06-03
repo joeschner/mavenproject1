c330c09bedd938f53f0d85d1099888efc24069cf
1-2e <holfeldj@gmail.com>
2024-06-03 13:32:27 +0200

    Add files via upload

---
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject4;
+
+/**
+ *
+ * @author 14237077
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
