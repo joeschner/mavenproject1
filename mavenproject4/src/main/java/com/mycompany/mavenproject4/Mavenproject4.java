92052a3adca4529a9e651480c56c5e38f1220871
14237050 <14237050@Terra5.Fortis.local>
2024-06-03 13:38:59 +0200

    oke

--- mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject4;
 public class Mavenproject4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hehehehe!");
     }
 }
